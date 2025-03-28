d13b2dee5c8329861f9cea2ab0fe3c46338bcf89
Regina7411 <danielagg71411@gmail.com>
2025-03-28 04:28:51 +0000

    estructura correcta archivos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-import "bootstrap";
-import "./style.css";
+const palos = ['spade', 'club', 'heart', 'diamond'];
+const valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Rey', 'Reina', 'Jota', 'As'];
 
+function generarCarta() {
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+    const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    const carta = `
+        <div class="card-top">${valorAleatorio}</div>
+        <div class="card-value ${paloAleatorio}">
+            <span class="card-suit">${getSimboloPalo(paloAleatorio)}</span>
+        </div>
+        <div class="card-bottom">${valorAleatorio}</div>
+    `;
+    
+    return carta;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getSimboloPalo(palo) {
+    switch(palo) {
+        case 'heart':
+            return '♥';
+        case 'spade':
+            return '♠';
+        case 'club':
+            return '♣';
+        case 'diamond':
+            return '♦';
+        default:
+            return '';
+    }
+}
+
+function mostrarCarta() {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = generarCarta();
+}
+
+mostrarCarta();
+
+setInterval(mostrarCarta, 10000);
